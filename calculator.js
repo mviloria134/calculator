7bf566919bf70b5231fa9213a15811070835f7f1
Maria Viloria <mviloria134@gmail.com>
2026-09-14 18:15:31 -0700

    Create function that does a selected operation on two numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+
+        case "-":
+            return subtract(num1, num2);
+
+        case "*":
+            return multiply(num1, num2);
+        
+        case "/":
+            return divide(num1, num2);
+    
+        default:
+            break;
+    }
 }
